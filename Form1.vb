8731002ea5f2d43079a00e41aeef0c622ce78ace
Sondre Garli <sondregarli93@hotmail.com>
2015-10-27 14:02:43 +0100

    abc

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim lyd As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play(My.Resources.Upisdown,
+          AudioPlayMode.BackgroundLoop)
+        lyd = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If lyd = True Then
+            My.Computer.Audio.Stop()
+            lyd = False
+        Else
+            My.Computer.Audio.Play(My.Resources.Upisdown,
+          AudioPlayMode.BackgroundLoop)
+            lyd = True
+        End If
+    End Sub
+End Class
